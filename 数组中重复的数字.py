9c41b81ba28eb315c35af408ba0642c810949ba5
Snowing-ST <404011463@qq.com>
2018-05-04 12:53:01 +0800

    增加交换数字的方法，但在牛客网没通过

--- 数组中重复的数字.py
+++ 数组中重复的数字.py
@@ -18,11 +18,29 @@ def duplicate(numbers,duplication):
         d[i]=True
     return False
         
-#方法一：简单的排序
+#方法一：简单的排序后进行遍历
 def duplicate(numbers,duplication):
     #此处写排序算法
+    pass
 
 
-#方法三：交换数字
+#方法三：交换数字,未通过
 def duplicate(numbers,duplication):
+    i=0
+    while i<len(numbers):
+        if i<numbers[i]:
+            t = numbers[numbers[i]]
+            numbers[numbers[i]] = numbers[i]
+            numbers[i] = t
+        elif i==numbers[i]:
+            i=i+1
+        else:
+            print(numbers[i])
+            duplication[0] = numbers[i]
+            return True
+    return False
+
+numbers = [2,3,1,0,2,5,3]
+duplication=[0]
+
     
