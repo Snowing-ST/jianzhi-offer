2633605dfad1104bfc05272d48e0a00df92b0320
Snowing-ST <404011463@qq.com>
2018-05-08 15:35:24 +0800

    update

--- 15 二进制中1的个数.py
+++ 15 二进制中1的个数.py
@@ -8,3 +8,11 @@ Created on Tue May  8 13:42:28 2018
 @author: situ
 """
 
+#书上的代码1 可能陷入死循环
+def NumberOf1(n):
+    count = 0
+    while n:
+        if n & 1: #判断n的二进制的末尾是不是1
+            count+=1 #是的话就加1
+            n=n>>1 #n转化为二进制后右移一位，又转化成10进制
+    return count
